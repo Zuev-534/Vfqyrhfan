96575656e4387d854d78f147500a0177e222f478
Zuev-534 <zuev,andrey.2103@g,ail.com>
2021-11-23 16:30:36 +0300

    Камера становится дочерний

--- Camera.py
+++ Camera.py
@@ -1,15 +1,12 @@
 from vocabulary import *
+from graph import *
 
-class Camera:
-    def __init__(self, x0=0, y0=0, v0 = 0, an_xy=0, an_xz=0, h=1.75, pr_pl_rng=0.3):
-        self.x = x0
-        self.y = y0
+class Camera(Vector):
+    def __init__(self, v0 = 0, h=1.75):
+        super(Camera, self).__init__()
         self.vx = v0
         self.vy = v0
-        self.an_xy = an_xy
-        self.an_xz = an_xz
         self.h = h
-        self.focus = pr_pl_rng
 
     def control(self, order, an_xz = 0, an_xy = 0):
         """
@@ -52,24 +49,60 @@ def coords(screen, cam):
     text_render(screen, "an_xz = " + str(cam.an_xz), 50, 200)
 
 
+
 def interoperate(input_movement):
     if input_movement.type == pygame.QUIT:
         pygame.quit()
     elif input_movement.type == pygame.KEYDOWN:
         if input_movement.key == pygame.K_ESCAPE:
             pygame.quit()
-        elif input_movement.key == pygame.K_a:
-            return LEFT, 0, 0
-        elif input_movement.key == pygame.K_d:
-            return RIGHT, 0, 0
-        elif input_movement.key == pygame.K_s:
-            return BACKWARD, 0, 0
-        elif input_movement.key == pygame.K_w:
-            return FORWARD, 0, 0
-    elif input_movement.type == pygame.MOUSEMOTION:
+    if input_movement.type == pygame.MOUSEMOTION:
         x, y = pygame.mouse.get_pos()
         x, y = - k * (x - int(WIDTH / 2)), k * (y - int(HEIGHT / 2)) # x = - delta <= ось с пайгейиои не сходится
         pygame.mouse.set_pos([int(WIDTH / 2), int(HEIGHT / 2)])
         return ROTATE, x, y
-    else:
-        return NOTHING, 0, 0
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        return FORWARD, 0, 0
+    elif keys[pygame.K_s]:
+        return BACKWARD, 0, 0
+    elif keys[pygame.K_a]:
+        return LEFT, 0, 0
+    elif keys[pygame.K_d]:
+        return RIGHT, 0, 0
+    return NOTHING, 0, 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Обработка действий на нажатие кнопки
+# def interoperate(input_movement):
+#     if input_movement.type == pygame.QUIT:
+#         pygame.quit()
+#     elif input_movement.type == pygame.KEYDOWN:
+#         if input_movement.key == pygame.K_ESCAPE:
+#             pygame.quit()
+#         elif input_movement.key == pygame.K_a:
+#             return LEFT, 0, 0
+#         elif input_movement.key == pygame.K_d:
+#             return RIGHT, 0, 0
+#         elif input_movement.key == pygame.K_s:
+#             return BACKWARD, 0, 0
+#         elif input_movement.key == pygame.K_w:
+#             return FORWARD, 0, 0
+#     elif input_movement.type == pygame.MOUSEMOTION:
+#         x, y = pygame.mouse.get_pos()
+#         x, y = - k * (x - int(WIDTH / 2)), k * (y - int(HEIGHT / 2)) # x = - delta <= ось с пайгейиои не сходится
+#         pygame.mouse.set_pos([int(WIDTH / 2), int(HEIGHT / 2)])
+#         return ROTATE, x, y
+#     else:
+#         return NOTHING, 0, 0
