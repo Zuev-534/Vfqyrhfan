fd7ba76691980cd521ef67232a03da0a97a5319d
Zuev-534 <zuev,andrey.2103@g,ail.com>
2021-12-09 20:12:21 +0300

    Вращение переписано через numba

--- graph/vector.py
+++ graph/vector.py
@@ -1,55 +1,35 @@
 from __future__ import annotations
 from vocabulary import *
+from numba import njit
+import numpy as np
+import math
 
 
-# from numba import njit
-#
-#
-#
-# @njit(cache=True, parallel=True, fastmath=True)
-# def get_vector(a, d_a, kam, d_kam, an_xy, an_xz, D):
-#     d_a = a - kam
-#     d_kam[0] = D * np.cos(an_xy) * np.cos(an_xz)
-#     d_kam[1] = D * np.sin(an_xy) * np.cos(an_xz)
-#     d_kam[2] = D * np.sin(an_xz)
-#     l = (np.sum(d_kam * d_a) / np.square(D))
-#     d_a = d_a / l - d_kam
-#     return (d_a, an_xy, an_xz, D)
-#
-# # @njit(cache=True, parallel=True, fastmath=True)
-# def coords_to_cam(d_a, an_xy, an_zx, D, WIDTH=800, HEIGHT=450):
-#     x = d_a[0]
-#     y = d_a[1]
-#     d_a[0] = x * cos(-an_xy) - y * sin(-an_xy)
-#     d_a[1] = x * sin(-an_xy) + y * cos(-an_xy)
-#     x = d_a[0]
-#     z = d_a[2]
-#     d_a[0] = x * cos(an_zx) + z * sin(an_zx)
-#     d_a[2] = - x * sin(an_zx) + z * cos(an_zx)
-#     x = d_a[0]
-#     y = d_a[1]
-#     d_a[0] = - y
-#     d_a[1] = x
-#
-#     return ((d_a[0] * WIDTH / 2 / D + WIDTH / 2), (d_a[2] * HEIGHT / 2 * sqrt(3) / D + HEIGHT / 2))
-#
 
-# def get_vector(self, self.d, kam, d_kam, an_xy, an_xz):
-#     self.dx = -vector_nul.x + self.x
-#     self.dy = -vector_nul.y + self.y
-#     self.dz = -vector_nul.z + self.z
-#     self.d = sqrt(self.dx ** 2 + self.dy ** 2 + self.dz ** 2)
-#     vector_nul.dx = vector_nul.d * cos(vector_nul.an_xy) * cos(vector_nul.an_xz)
-#     vector_nul.dy = vector_nul.d * sin(vector_nul.an_xy) * cos(vector_nul.an_xz)
-#     vector_nul.dz = vector_nul.d * sin(vector_nul.an_xz)
-#     self.get_angle_cos(vector_nul)
-#     l = (self.dx * vector_nul.dx + self.dy * vector_nul.dy + self.dz * vector_nul.dz) / (vector_nul.d * vector_nul.d)
-#     dx = self.dx / l - vector_nul.dx
-#     dy = self.dy / l - vector_nul.dy
-#     dz = self.dz / l - vector_nul.dz
-#     abc = Vector(dx0=dx, dy0=dy, dz0=dz)
-#     abc.set_coords_d_from_di()
-#     return abc
+
+
+
+
+
+@njit(fastmath=True)
+def r_v_z(x, y, fi_xy=0.0):
+    return x * cos(fi_xy) - y * sin(fi_xy), x * sin(fi_xy) + y * cos(fi_xy)
+
+@njit(fastmath=True)
+def r_v_y(x, z, fi_zx=0.0):
+    return x * cos(fi_zx) + z * sin(fi_zx), - x * sin(fi_zx) + z * cos(fi_zx)
+
+@njit(fastmath=True)
+def r_v_x(z, y, fi_yz=0.0):
+    return y * sin(fi_yz) + z * cos(fi_yz), y * cos(fi_yz) - z * sin(fi_yz)
+
+
+
+
+
+
+
+
 
 
 class Vector:
@@ -129,22 +109,13 @@ class Vector:
         return abc
 
     def rotate_vector_z(self, fi_xy=0.0):
-        x = self.dx
-        y = self.dy
-        self.dx = x * cos(fi_xy) - y * sin(fi_xy)
-        self.dy = x * sin(fi_xy) + y * cos(fi_xy)
+        self.dx, self.dy = r_v_z(self.dx, self.dy, fi_xy)
 
     def rotate_vector_y(self, fi_zx=0.0):
-        x = self.dx
-        z = self.dz
-        self.dx = x * cos(fi_zx) + z * sin(fi_zx)
-        self.dz = - x * sin(fi_zx) + z * cos(fi_zx)
+        self.dx, self.dz = r_v_y(self.dx, self.dz, fi_zx)
 
     def rotate_vector_x(self, fi_yz=0.0):
-        z = self.dz
-        y = self.dy
-        self.dz = y * sin(fi_yz) + z * cos(fi_yz)
-        self.dy = y * cos(fi_yz) - z * sin(fi_yz)
+        self.dz, self.dy = r_v_x(self.dz, self.dy, fi_yz)
 
     def print_all(self):
         print("self = ", self)
